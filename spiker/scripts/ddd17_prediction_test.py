5e46d36716a2f51fc5e1e9e7b77be13ecbe16eea
Yuhuang Hu <duguyue100@gmail.com>
2017-11-10 17:44:29 +0100

    update prediction test

--- spiker/scripts/ddd17_prediction_test.py
+++ spiker/scripts/ddd17_prediction_test.py
@@ -15,8 +15,8 @@ from spiker.data import ddd17
 
 # load and process data
 data_path = os.path.join(spiker.SPIKER_DATA, "ddd17",
-                         "jul09/rec1499657850-export.hdf5")
-frame_cut = [500, 500]
+                         "aug09/rec1502336427-export.hdf5")
+frame_cut = [100, 400]
 frames, steering = ddd17.prepare_train_data(
     data_path, y_name="steering",
     frame_cut=frame_cut)
@@ -30,11 +30,11 @@ X_test = frames[num_train:]
 Y_test = steering[num_train:]
 del frames, steering
 
-model_name_base = "steering-night-2-"
+model_name_base = "steering-night-6-"
 # load model
 model_path = os.path.join(
     spiker.SPIKER_EXPS, model_name_base+"full",
-    model_name_base+"full-00-0.94.hdf5")
+    model_name_base+"full-177-0.00.hdf5")
 model = utils.keras_load_model(model_path)
 aps_dvs_prediction = utils.keras_predict_batch(model, X_test, verbose=True)
 x_axis = np.arange(X_test.shape[0])
@@ -42,7 +42,7 @@ x_axis = np.arange(X_test.shape[0])
 #  load model
 model_path = os.path.join(
     spiker.SPIKER_EXPS, model_name_base+"dvs",
-    model_name_base+"dvs-93-0.45.hdf5")
+    model_name_base+"dvs-66-0.03.hdf5")
 model = utils.keras_load_model(model_path)
 dvs_prediction = utils.keras_predict_batch(
     model, X_test[..., 0][..., np.newaxis], verbose=True)
@@ -50,20 +50,20 @@ dvs_prediction = utils.keras_predict_batch(
 # load model
 model_path = os.path.join(
     spiker.SPIKER_EXPS, model_name_base+"aps",
-    model_name_base+"aps-01-1.03.hdf5")
+    model_name_base+"aps-104-0.01.hdf5")
 model = utils.keras_load_model(model_path)
 aps_prediction = utils.keras_predict_batch(
     model, X_test[..., 1][..., np.newaxis], verbose=True)
 
 # plot figure
 plt.figure(figsize=(10, 4))
-plt.plot(x_axis, Y_test, "r", label="ground truth")
-plt.plot(x_axis, aps_dvs_prediction, "g", label="DVS+APS")
-plt.plot(x_axis, dvs_prediction, "b", label="DVS")
-plt.plot(x_axis, aps_prediction, "orange", label="APS")
+plt.plot(x_axis, Y_test, "r", label="ground truth", linewidth=1)
+plt.plot(x_axis, aps_dvs_prediction, "g", label="DVS+APS", linewidth=1)
+plt.plot(x_axis, dvs_prediction, "b", label="DVS", linewidth=1)
+plt.plot(x_axis, aps_prediction, "orange", label="APS", linewidth=1)
 plt.title("night 2")
 plt.xlabel("frames")
 plt.ylabel("radian")
 plt.legend()
-plt.savefig(os.path.join(spiker.SPIKER_EXTRA, "night-2-steering.png"),
+plt.savefig(os.path.join(spiker.SPIKER_EXTRA, "night-6-steering.png"),
             dpi=200, format="png")
