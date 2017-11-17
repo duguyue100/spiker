5c8820448b2cb2e4a1a909e3d075314edcd3820c
Yuhuang Hu <duguyue100@gmail.com>
2017-11-17 18:19:37 +0100

    update stats figure

--- spiker/scripts/cvpr_create_figures.py
+++ spiker/scripts/cvpr_create_figures.py
@@ -17,6 +17,61 @@ import spiker
 from spiker.models import utils
 from spiker.data import ddd17
 
+
+def compute_log_curve(log_name, num_runs=4):
+    """Compute log curve, provide mean and standard deviation."""
+    log_collector = []
+    for run_idx in range(1, num_runs+1):
+        # prepare log path
+        log_file = join(spiker.SPIKER_EXPS+"-run-%d" % (run_idx),
+                        log_name, "csv_history.log")
+        log_dict = utils.parse_csv_log(log_file)
+        log_collector.append(log_dict)
+
+    # compute for train loss
+    train_loss = np.vstack(
+        (log_collector[0]["loss"][np.newaxis, ...],
+         log_collector[1]["loss"][np.newaxis, ...],
+         log_collector[2]["loss"][np.newaxis, ...],
+         log_collector[3]["loss"][np.newaxis, ...]))
+    train_loss_mean = np.mean(train_loss, axis=0)
+    train_loss_std = np.std(train_loss, axis=0)
+
+    # compute for test loss
+    test_loss = np.vstack(
+        (log_collector[0]["val_loss"][np.newaxis, ...],
+         log_collector[1]["val_loss"][np.newaxis, ...],
+         log_collector[2]["val_loss"][np.newaxis, ...],
+         log_collector[3]["val_loss"][np.newaxis, ...]))
+    test_loss_mean = np.mean(test_loss, axis=0)
+    test_loss_std = np.std(test_loss, axis=0)
+
+    # compute for train mse
+    train_mse = np.vstack(
+        (log_collector[0]["mean_squared_error"][np.newaxis, ...],
+         log_collector[1]["mean_squared_error"][np.newaxis, ...],
+         log_collector[2]["mean_squared_error"][np.newaxis, ...],
+         log_collector[3]["mean_squared_error"][np.newaxis, ...]))
+    train_mse_mean = np.mean(train_mse, axis=0)
+    train_mse_std = np.std(train_mse, axis=0)
+
+    # compute for test mse
+    test_mse = np.vstack(
+        (log_collector[0]["val_mean_squared_error"][np.newaxis, ...],
+         log_collector[1]["val_mean_squared_error"][np.newaxis, ...],
+         log_collector[2]["val_mean_squared_error"][np.newaxis, ...],
+         log_collector[3]["val_mean_squared_error"][np.newaxis, ...]))
+    test_mse_mean = np.mean(test_mse, axis=0)
+    test_mse_std = np.std(test_mse, axis=0)
+
+    trloss = (train_loss_mean, train_loss_std)
+    teloss = (test_loss_mean, test_loss_std)
+    trmse = (train_mse_mean, train_mse_std)
+    temse = (test_mse_mean, test_mse_std)
+
+    return trloss, teloss, trmse, temse
+
+
 def get_best_result(log_dict, mode="regress"):
     """Get result from a list of log files."""
     logs = OrderedDict()
@@ -55,7 +110,8 @@ def get_log_file_dict(env="day", mode="full", task="steering",
 #  option = "get-results-reproduce"
 #  option = "get-results-reproduce-steer"
 #  option = "get-steering-results"
-option = "attribute-hist"
+#  option = "attribute-hist"
+option = "get-steer-loss-curves"
 
 if option == "get-full-results":
     steer_day_logs = get_log_file_dict("day", "full", "steering")
@@ -765,3 +821,102 @@ elif option == "attribute-hist":
                      attribute+".pdf"),
                 dpi=600, format="pdf",
                 pad_inches=0.5)
+elif option == "get-steer-loss-curves":
+    # collect curves
+    for env in ["night", "day"]:
+        env_range = 7 if env == "night" else 8
+        for env_idx in xrange(env_range):
+            # create grid specs
+            log_name = "steering"+"-"+env+"-%d-" % (env_idx+1)
+            full_log_name = "steering"+"-"+env+"-%d-" % (env_idx+1)+"full"
+            dvs_log_name = "steering"+"-"+env+"-%d-" % (env_idx+1)+"dvs"
+            aps_log_name = "steering"+"-"+env+"-%d-" % (env_idx+1)+"aps"
+
+            full_log = compute_log_curve(full_log_name)
+            dvs_log = compute_log_curve(dvs_log_name)
+            aps_log = compute_log_curve(aps_log_name)
+
+            full_axis = range(1, full_log[0][0].shape[0]+1)
+            dvs_axis = range(1, dvs_log[0][0].shape[0]+1)
+            aps_axis = range(1, aps_log[0][0].shape[0]+1)
+
+            # produce figure 
+            fig = plt.figure(figsize=(16, 8))
+
+            print (full_log[0][0].shape)
+
+            # train loss
+            trloss_ax = fig.add_subplot(221)
+            trloss_ax.plot(full_axis, full_log[0][0], label="DVS+APS",
+                           color="#08306b", linestyle="-", linewidth=2)
+            trloss_ax.plot(dvs_axis, dvs_log[0][0], label="DVS",
+                           color="#7f2704", linestyle="-", linewidth=2)
+            trloss_ax.plot(aps_axis, aps_log[0][0], label="APS",
+                           color="#3f007d", linestyle="-", linewidth=2)
+            plt.xlabel("epochs", fontsize=15)
+            plt.ylabel("loss", fontsize=15)
+            plt.title("Training Loss")
+            plt.xticks(fontsize=15)
+            plt.yticks(fontsize=15)
+            plt.grid(linestyle="-.")
+            plt.yscale("log")
+            plt.legend(fontsize=15, shadow=True)
+
+            # test loss
+            teloss_ax = fig.add_subplot(222)
+            teloss_ax.plot(full_axis, full_log[1][0], label="DVS+APS",
+                           color="#08306b", linestyle="-", linewidth=2)
+            teloss_ax.plot(dvs_axis, dvs_log[1][0], label="DVS",
+                           color="#7f2704", linestyle="-", linewidth=2)
+            teloss_ax.plot(aps_axis, aps_log[1][0], label="APS",
+                           color="#3f007d", linestyle="-", linewidth=2)
+            plt.xlabel("epochs", fontsize=15)
+            plt.ylabel("loss", fontsize=15)
+            plt.title("Testing Loss")
+            plt.xticks(fontsize=15)
+            plt.yticks(fontsize=15)
+            plt.grid(linestyle="-.")
+            plt.yscale("log")
+            plt.legend(fontsize=15, shadow=True)
+
+            # train mse
+            trmse_ax = fig.add_subplot(223)
+            trmse_ax.plot(full_axis, full_log[2][0], label="DVS+APS",
+                          color="#08306b", linestyle="-", linewidth=2)
+            trmse_ax.plot(dvs_axis, dvs_log[2][0], label="DVS",
+                           color="#7f2704", linestyle="-", linewidth=2)
+            trmse_ax.plot(aps_axis, aps_log[2][0], label="APS",
+                           color="#3f007d", linestyle="-", linewidth=2)
+            plt.xlabel("epochs", fontsize=15)
+            plt.ylabel("mse", fontsize=15)
+            plt.title("Training MSE")
+            plt.xticks(fontsize=15)
+            plt.yticks(fontsize=15)
+            plt.grid(linestyle="-.")
+            plt.yscale("log")
+            plt.legend(fontsize=15, shadow=True)
+
+            # test mse
+            temse_ax = fig.add_subplot(224)
+            temse_ax.plot(full_axis, full_log[3][0], label="DVS+APS",
+                          color="#08306b", linestyle="-", linewidth=2)
+            temse_ax.plot(dvs_axis, dvs_log[3][0], label="DVS",
+                           color="#7f2704", linestyle="-", linewidth=2)
+            temse_ax.plot(aps_axis, aps_log[3][0], label="APS",
+                           color="#3f007d", linestyle="-", linewidth=2)
+            plt.xlabel("epochs", fontsize=15)
+            plt.ylabel("mse", fontsize=15)
+            plt.title("Testing MSE")
+            plt.xticks(fontsize=15)
+            plt.yticks(fontsize=15)
+            plt.grid(linestyle="-.")
+            plt.yscale("log")
+            plt.legend(fontsize=15, shadow=True)
+
+            plt.tight_layout()
+
+            # save figure
+            plt.savefig(join(spiker.SPIKER_EXTRA, "cvprfigs",
+                             log_name+".pdf"),
+                        dpi=600, format="pdf",
+                        bbox="tight", pad_inches=0.5)
